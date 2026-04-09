8d75816ab3ec94e70c61638f6e570dba611d3c6f
gabrielle-lima <gaibou_lima@hotmail.com>
2026-04-09 01:19:57 -0300

    feat: Criação de enum, estruturação das pastas, criação de repositories/controllers e DTOs para requests/responses

---
+++ src/main/java/enums/StatusOcorrencia.java
@@ -0,0 +1,8 @@
+package enums;
+
+public enum StatusOcorrencia {
+
+	ATIVA,FINALIZADA;
+
+
+}
---
+++ src/main/java/repositories/OcorrenciaRepository.java
@@ -0,0 +1,9 @@
+package repositories;
+
+import entities.Ocorrencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OcorrenciaRepository extends JpaRepository<Ocorrencia, Long> {
+}
