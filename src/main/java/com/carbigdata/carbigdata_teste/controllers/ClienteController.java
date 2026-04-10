13e61269d7b0161d31b708c2d1e2ab67a8c1c867
gabrielle-lima <gaibou_lima@hotmail.com>
2026-04-10 17:07:33 -0300

    feat: Criado dockerfile

--- src/main/java/com/carbigdata/carbigdata_teste/controllers/ClienteController.java
+++ src/main/java/com/carbigdata/carbigdata_teste/controllers/ClienteController.java
@@ -1,5 +1,6 @@
 package com.carbigdata.carbigdata_teste.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import com.carbigdata.carbigdata_teste.services.ClienteService;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,12 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ClienteController {
 
-	private final ClienteService clienteService;
+	@Autowired
+	private ClienteService clienteService;
 
 
-	public ClienteController(ClienteService clienteService) {
-		this.clienteService = clienteService;
-	}
 
 
 
